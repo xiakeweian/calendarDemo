4c0a3c76c657368ea6a1a9c70abacca01dfff5bc
wuyanxia <wuyanxia@costrip.cn>
2024-07-25 15:57:22 +0800

    feat: 添加外层控制日期变化

--- home/index.js
+++ home/index.js
@@ -1,3 +1,4 @@
+const dayjs = require('../utils/dayjs.js');
  Page({
   data: {
    
@@ -18,15 +19,15 @@
       y2024m7d23: 'deep-spot',
     },
     // 例子，今天之后的日期不能被选中
-    // disabledDate({ day, month, year }) {
-    //   const now = new Date();
-    //   const date = new Date(year, month - 1, day);
-    //   return date > now;
-    // },
+    disabledDate({ day, month, year }) {
+      const now = new Date();
+      const date = new Date(year, month - 1, day);
+      return date > now;
+    },
     // 需要改变日期时所使用的字段
     changeTime: '',
     selectPopModalEle: null,
-    selectDate: '',
+    selectDate: dayjs(new Date()).format('YYYY/MM/DD'),
     tempSelectDate: {}
   },
   /**
@@ -47,19 +48,17 @@
     console.log(detail, '下拉展开收回');
     
   },
-  changetime() {
-    this.setData({
-      changeTime: '2022/1/1',
-    });
-  },
   handleSelectDate() {
     this.data.selectPopModalEle?.onShow();
+    this.setData({
+      changeTime: this.data.selectDate
+    });
   },
   // 返回
   handleConfirm() {
     const { tempSelectDate } = this.data;
     this.setData({
-      selectDate: `${tempSelectDate.year}/${tempSelectDate.month}/${tempSelectDate.day}`
+      selectDate: dayjs(`${tempSelectDate.year}/${tempSelectDate.month}/${tempSelectDate.day}`).format('YYYY/MM/DD')
     })
     this.data.selectPopModalEle?.setData({ isShow: false });
 
@@ -67,5 +66,38 @@
   handleCloseModal() {
 
   },
+  handleChangeDate(e) {
+    console.log(e, 'gssss');
+    const { type } = e.currentTarget.dataset;
+    const { selectDate } = this.data;
+    console.log(selectDate, selectDate.split('/')[0], 'selectDate')
+
+    const date = this.getComputeDay(+selectDate.split('/')[0], +selectDate.split('/')[1], +selectDate.split('/')[2],type);
+    console.log(date, dayjs(date).isAfter(dayjs().startOf('day')), 'gssss');
+    if(dayjs(date).isAfter(dayjs().startOf('day'))){
+      wx.showToast({ title: '不可选择今天之后的日期', icon: 'none' }).then();
+      return;
+    }
+    this.setData({
+      selectDate: dayjs(date).format('YYYY/MM/DD')
+    });
+
+  },
+    /**
+   * 计算前一天 / 后一天
+   * @param year 年
+   * @param month 月
+   * @param day 日
+   * @param type 类型 pre 前一天 next 后一天
+   */
+    getComputeDay(year, month, day, type) {
+      const date = new Date(year, month-1, day); // 月份是从0开始的，所以减1
+      if(type === 'pre'){
+        date.setDate(date.getDate() - 1); // 减去一天
+      }else{
+        date.setDate(date.getDate() + 1); // 加一天
+      }
+      return date.getFullYear() + '/' + (date.getMonth() + 1).toString().padStart(2, '0') + '/' + date.getDate().toString().padStart(2, '0');
+    },
  
 })
